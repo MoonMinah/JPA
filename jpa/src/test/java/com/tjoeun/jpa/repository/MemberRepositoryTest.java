b476c7be2d7e95a44a66adf7622163f0dd034a0c
MoonMinah <133863745+MoonMinah@users.noreply.github.com>
2023-11-20 14:53:19 +0900

    Add files via upload

---
+++ jpa/src/test/java/com/tjoeun/jpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.tjoeun.jpa.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.tjoeun.jpa.domain.Member;
+
+//	@SpringBootTest 어노테이션을 붙여서 테스트 클래스임을 springBoot에게 알려준다.
+@SpringBootTest
+class MemberRepositoryTest {
+
+	@Autowired
+	private MemberRepository memberRepository;
+	
+	@Test
+	public void crud() {
+		Member member = new Member("홍길동", "골뱅이");
+		memberRepository.save(member);
+		
+//		System.out.println(memberRepository.findAll());
+//		여러 줄로 출력한다.
+//		for (Member m : memberRepository.findAll()) {
+//			System.out.println(m);
+//		}
+//		람다 식을 사용해서 여러 줄로 출력한다.
+		memberRepository.findAll().forEach(System.out::println);
+	}
+
+}
